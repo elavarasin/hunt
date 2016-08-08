b3e68e3fea23beecf9d0bcdf5751b239854a7426
elavarasin <elavarasi1596@gmail.com>
2016-08-08 02:26:37 -0700

    sum1.java

---
+++ sum1.java
@@ -0,0 +1,18 @@
+//1234=1+3+6+10
+public class sum1 {
+	public static void main(String[] args) {
+		int c=Integer.parseInt(args[0]);
+		int rev=0,b,sum=0;
+	while(c!=0){
+		rev=c;
+		while(rev!=0){
+			b=rev%10;
+			sum=sum+b;
+			rev=rev/10;
+			}
+		c=c/10;
+	}
+	System.out.println(sum);
+}	
+		
+}	
